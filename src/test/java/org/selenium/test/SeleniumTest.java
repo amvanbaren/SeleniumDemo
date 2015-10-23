f8f8381a20a082c0e857cd86802b25109a2e667e
amvanbaren <amvanbaren@hotmail.com>
2015-10-23 20:02:58 +0200

    changed test setup

--- src/test/java/org/selenium/test/SeleniumTest.java
+++ src/test/java/org/selenium/test/SeleniumTest.java
@@ -77,19 +77,27 @@ public class SeleniumTest {
 	@Test
 	public void test() {
 	      // Click on Math Calculators
-	      driver.findElement(By.xpath(".//*[@id='menu']/div[3]/a")).click();     	
+	      driver.findElement(By.xpath(".//*[@id='menu']/div[3]/a")).click();   
+	      System.out.println("CLICK | .//*[@id='menu']/div[3]/a");
 	      // Click on Percent Calculators
 	      driver.findElement(By.xpath(".//*[@id='menu']/div[4]/div[3]/a")).click();
+	      System.out.println("CLICK | .//*[@id='menu']/div[4]/div[3]/a");
+
 	      // Enter value 10 in the first number of the percent Calculator
 	      driver.findElement(By.id("cpar1")).sendKeys("10");
+	      System.out.println("SEND KEYS | cpar1, 10");
+
 	      // Enter value 50 in the second number of the percent Calculator
 	      driver.findElement(By.id("cpar2")).sendKeys("50");
+	      System.out.println("SEND KEYS | cpar2, 50");
+
 	      
 	      // Click Calculate Button driver.findElement(By.xpath(".//*[@id='content']/table/tbody/tr/td[2]/input")).click();
 	      // Get the Result Text based on its xpath
 	      String result = driver.findElement(By.xpath(".//*[@id='content']/p[2]/span/font/b")).getText();
 	      // Print a Log In message to the screen
 	      System.out.println(" The Result is " + result);
+	      
 	      if(result.equals("5"))
 	      {
 	         System.out.println(" The Result is Pass");
