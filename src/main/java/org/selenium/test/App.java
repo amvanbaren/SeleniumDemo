435b8de96a695ce37eab053d824ba74ba8c72ae0
amvanbaren <amvanbaren@hotmail.com>
2015-10-30 16:26:30 +0100

    Test webhook

--- src/main/java/org/selenium/test/App.java
+++ src/main/java/org/selenium/test/App.java
@@ -3,6 +3,6 @@ package org.selenium.test;
 public class App 
 {
 	public static void main(String[] args) throws Exception {                    
-	      return "Hello World!";    
+	      System.out.println("Hello World!");    
 	}
 }
